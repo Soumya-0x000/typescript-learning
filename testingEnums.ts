f06f59120baf562d31c166b60feda4808f692e92
Soumya-0x000 <soumyadas429@gmail.com>
2024-07-06 23:57:32 +0530

    updated with enums

---
+++ testingEnums.ts
@@ -0,0 +1,34 @@
+enum Size { small = 10, medium, large }
+let mySize: Size = Size.large
+console.log(mySize)
+
+enum Color {
+    Navy,
+    Red = 'red',
+    Green = 'green',
+    Blue = 'blue',
+    Orange = 'orange',
+    Purple = 'purple',
+    Yellow = 'yellow',
+    Pink = 'pink',
+    Cyan = 'cyan',
+    Magenta = 'magenta',
+    Maroon = 'maroon',
+    Aqua = 'aqua',
+    Lime = 'lime',
+    Teal = 'teal',
+    Fuchsia = 'fuchsia',
+}
+console.log(Color.Navy)
+
+let myColor: Color = Color.Green
+
+Object.values(Color).includes(myColor) 
+    ? console.log('valid color')
+    : console.log('invalid color')
+
+const colorObj: {[key: string]: any} = {}
+colorObj.seaGreen = 'Sea Green'
+colorObj.five = 5
+colorObj.isTrue = true
+console.log(colorObj)
